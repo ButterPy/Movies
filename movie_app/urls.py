7f6dd3ebfdd3e2188ceb89d5dc0d7c2872831f79
Butya <mydearseliisa@gmail.com>
2022-05-23 12:47:41 +0600

    adding slug

---
+++ movie_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+
+urlpatterns = [
+    path('', views.show_all_movie),
+    path('movie/<int:id_movie>', views.show_one_movie)
+
+]
---
+++ movie_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Movie
+
+def show_all_movie(request):
+    movies = Movie.objects.all
+    return render(request, 'movie.html', {
+        'movies': movies
+    })
+
+
+def show_one_movie(request, id_movie:int):
+    movie = Movie.objects.get(id = id_movie)
+    return render(request, 'movie.html', {
+        'movie': movie
+    })
+
