d619d8f73fa3291376fe0e5a7d42c3d4635761c1
Butya <mydearseliisa@gmail.com>
2022-05-23 13:55:21 +0600

    slug save

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from .models import Movie
 
+
 def show_all_movie(request):
-    movies = Movie.objects.all
+    movies = Movie.objects.all()
+    for movie in movies:
+        movie.save()
     return render(request, 'movie.html', {
         'movies': movies
     })
 
 
-def show_one_movie(request, id_movie:int):
-    movie = Movie.objects.get(id = id_movie)
+def show_one_movie(request, id_movie: int):
+    movie = Movie.objects.get(id=id_movie)
+
     return render(request, 'movie.html', {
         'movie': movie
     })
-
